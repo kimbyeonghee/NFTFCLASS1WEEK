744e922bd48bb7556cb5ddf76209827281efea0a
kimbyeonghee <55523990+kimbyeonghee@users.noreply.github.com>
2020-09-10 17:00:45 +0900

    Add files via upload

---
+++ 201987076_1/201987076_1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _201987076_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("학번: 201987076");
+            Console.WriteLine("이름: 김병희");
+        }
+    }
+}
